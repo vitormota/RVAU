19a2e4832f681258995c9414fce1564ecea0fea9
Pedro Costa <pedro.vendascosta@gmail.com>
2014-11-21 16:22:24 +0000

    Created Project and added main

---
+++ proj/main.cpp
@@ -0,0 +1,17 @@
+#include "opencv2/imgproc/imgproc.hpp"
+#include "opencv2/highgui/highgui.hpp"
+#include "opencv2/calib3d/calib3d.hpp"
+#include "opencv2/features2d/features2d.hpp"
+#include <iostream>
+#include <vector>
+
+using namespace cv;
+using namespace std;
+
+
+int main(int argc, char** argv){
+
+	cout << "Ola\n";
+
+	return 0;
+}
