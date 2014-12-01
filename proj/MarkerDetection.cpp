e752f4b4068f656847c6fff344fdbc8695d93f31
Pedro Costa <pedro.vendascosta@gmail.com>
2014-12-01 10:39:25 +0000

    Getting contours and finding the first two vertices of the contour. Going to do the same for finding the next two and then verify if it's a square.

--- proj/MarkerDetection.cpp
+++ proj/MarkerDetection.cpp
@@ -19,6 +19,17 @@ void binarizeImage(Mat img, Mat &dst){
 	threshold(img_gray, dst, THRESHOLD_VALUE, THRESHOLD_MAX_VALUE, THRESH_BINARY);
 }
 
+void verifySeedPoint(int &x, int &y, Mat img){
+	if( x < 0 )
+		x=0;
+	if( y < 0 )
+		y=0;
+	if( x >= img.cols )
+		x=img.cols-1;
+	if( y >= img.rows )
+		y=img.rows-1;
+}
+
 void findBlobs(Mat img, vector<KeyPoint> &keyPoints){
 	Mat out, binary, mask;
 
@@ -28,38 +39,69 @@ void findBlobs(Mat img, vector<KeyPoint> &keyPoints){
 
 	detector.detect( binary, keyPoints );
 	drawKeypoints( img, keyPoints, out, CV_RGB(0,255,0), DrawMatchesFlags::DEFAULT);
-	
-	
+
+
 	for( int i=0; i < keyPoints.size(); i++){
 		KeyPoint keyPoint = keyPoints.at(i);
-	
+
 		mask = Mat::zeros(binary.rows+2, binary.cols+2,CV_8U);	
 
 		for( int j=0; j < 10; j++){
 			int x = keyPoint.pt.x + (rand() % (int)keyPoint.size * PERCENTAGE_SIZE) - (keyPoint.size * PERCENTAGE_SIZE / 2 );
 			int y = keyPoint.pt.y + (rand() % (int)keyPoint.size * PERCENTAGE_SIZE) - (keyPoint.size * PERCENTAGE_SIZE / 2 );
+
+			verifySeedPoint(x,y,binary);			
+
 			floodFill(binary, mask, Point(x,y), 255, 0, Scalar(), Scalar(), 4+(255<<8)+FLOODFILL_MASK_ONLY);	
 		}
 		findBlobsContours(mask);
 	}
 }
 
+double distanceToLine(Point begin, Point end, Point x){
+	end-=begin;
+	x-=begin;
+
+	double area = x.cross(end);
+	return area / sqrt(end.x*end.x + end.y * end.y );
+}
+
 void findBlobsContours(cv::Mat img){
 	vector<vector<Point> > contours;
-    vector<Vec4i> hierarchy;
+	vector<Vec4i> hierarchy;
 
 	Mat dst = Mat::zeros(img.rows, img.cols, CV_8UC3);
 
-    findContours( img, contours, hierarchy, CV_RETR_TREE, CV_CHAIN_APPROX_SIMPLE );
+	findContours( img, contours, hierarchy, CV_RETR_EXTERNAL, CV_CHAIN_APPROX_SIMPLE );
 
-	// iterate through all the top-level contours,
-    // draw each connected component with its own random color
-    int idx = 0;
-	for( idx =0 ; idx < contours.size(); idx++ )
-    {
-        Scalar color( rand()&255, rand()&255, rand()&255 );
-        drawContours( dst, contours, idx, color, 1, 8, hierarchy );
-    }
+	for( int i=0; i < contours.size(); i++ ){
+		Scalar color( rand()&255, rand()&255, rand()&255 );
+		drawContours( dst, contours, i, color, 1, 8, hierarchy );
+		Point start =contours[i][0], end = contours[i][contours[i].size()/2];
+		cout << "Starting Point: " << start << ", Ending Point: " << end << endl;
+		line(dst,start,end,color,1);
+		double max =0, min=0;
+		Point maxPoint, minPoint;
+		for(int j=1; j < contours[i].size(); j++ ){
+			Point p = contours[i][j];
+			double d = distanceToLine(start,end,p);
+			if( d > max ){
+				max = d;
+				maxPoint = p;
+			}else if( d < min ){
+				min = d;
+				minPoint = p;
+			}
+		}
+
+		circle(dst,maxPoint,5,Scalar(0,0,255));
+		circle(dst,minPoint,5,Scalar(255,0,0));
+	}
+
+	cout << "Contours size: " << contours.size() << endl;
+
+	imshow("contours", dst);
+	waitKey();
 }
 
 SimpleBlobDetector::Params getBlobDetectorParams(){
