ca1404652703eb83c22402383bb550b750b7768b
Vitor Mota <vitormanuelmotasilva@gmail.com>
2014-12-13 21:47:05 +0000

    Performance tweaks.

--- proj/HomographyHelper.cpp
+++ proj/HomographyHelper.cpp
@@ -8,104 +8,116 @@
 using namespace cv;
 using namespace std;
 
+
 vector<Mat> markers;
 vector<Point2f> dstPoints;
 vector<vector<Point3f>> markerPoints;
 
 void initMarkerDatabase(){
-	markers.push_back(imread("marca1.png",CV_LOAD_IMAGE_GRAYSCALE));
-	markers.push_back(imread("marca2.png",CV_LOAD_IMAGE_GRAYSCALE));
-	markers.push_back(imread("marca3.png",CV_LOAD_IMAGE_GRAYSCALE));
-	markers.push_back(imread("marca4.png",CV_LOAD_IMAGE_GRAYSCALE));
+	markers.push_back(imread("marca1.png", CV_LOAD_IMAGE_GRAYSCALE));
+	markers.push_back(imread("marca2.png", CV_LOAD_IMAGE_GRAYSCALE));
+	markers.push_back(imread("marca3.png", CV_LOAD_IMAGE_GRAYSCALE));
+	markers.push_back(imread("marca4.png", CV_LOAD_IMAGE_GRAYSCALE));
 
 	vector<Point2f> marker1Points;
-	marker1Points.push_back(Point2f(0,0));
-	marker1Points.push_back(Point2f(MARKER_SIZE,0));
-	marker1Points.push_back(Point2f(MARKER_SIZE,MARKER_SIZE));
-	marker1Points.push_back(Point2f(0,MARKER_SIZE));
+	marker1Points.push_back(Point2f(0, 0));
+	marker1Points.push_back(Point2f(MARKER_SIZE, 0));
+	marker1Points.push_back(Point2f(MARKER_SIZE, MARKER_SIZE));
+	marker1Points.push_back(Point2f(0, MARKER_SIZE));
 
 	vector<Point2f> marker2Points;
-	marker2Points.push_back(Point2f(MARKER_SIZE,0));
-	marker2Points.push_back(Point2f(MARKER_SIZE,MARKER_SIZE));
-	marker2Points.push_back(Point2f(0,MARKER_SIZE));
-	marker2Points.push_back(Point2f(0,0));
+	marker2Points.push_back(Point2f(MARKER_SIZE, 0));
+	marker2Points.push_back(Point2f(MARKER_SIZE, MARKER_SIZE));
+	marker2Points.push_back(Point2f(0, MARKER_SIZE));
+	marker2Points.push_back(Point2f(0, 0));
 
 	vector<Point2f> marker3Points;
-	marker3Points.push_back(Point2f(MARKER_SIZE,MARKER_SIZE));
-	marker3Points.push_back(Point2f(0,MARKER_SIZE));
-	marker3Points.push_back(Point2f(0,0));
-	marker3Points.push_back(Point2f(MARKER_SIZE,0));
+	marker3Points.push_back(Point2f(MARKER_SIZE, MARKER_SIZE));
+	marker3Points.push_back(Point2f(0, MARKER_SIZE));
+	marker3Points.push_back(Point2f(0, 0));
+	marker3Points.push_back(Point2f(MARKER_SIZE, 0));
 
 	vector<Point2f> marker4Points;
-	marker4Points.push_back(Point2f(0,MARKER_SIZE));
-	marker4Points.push_back(Point2f(0,0));
-	marker4Points.push_back(Point2f(MARKER_SIZE,0));
-	marker4Points.push_back(Point2f(MARKER_SIZE,MARKER_SIZE));
+	marker4Points.push_back(Point2f(0, MARKER_SIZE));
+	marker4Points.push_back(Point2f(0, 0));
+	marker4Points.push_back(Point2f(MARKER_SIZE, 0));
+	marker4Points.push_back(Point2f(MARKER_SIZE, MARKER_SIZE));
 
 	vector<Point3f> marker1Points3;
-	marker1Points3.push_back(Point3f(0,0,0));
-	marker1Points3.push_back(Point3f(MARKER_SIZE,0,0));
-	marker1Points3.push_back(Point3f(MARKER_SIZE,MARKER_SIZE,0));
-	marker1Points3.push_back(Point3f(0,MARKER_SIZE,0));
+	marker1Points3.push_back(Point3f(0, 0, 0));
+	marker1Points3.push_back(Point3f(MARKER_SIZE, 0, 0));
+	marker1Points3.push_back(Point3f(MARKER_SIZE, MARKER_SIZE, 0));
+	marker1Points3.push_back(Point3f(0, MARKER_SIZE, 0));
 
 	vector<Point3f> marker2Points3;
-	marker2Points3.push_back(Point3f(MARKER_SIZE,0,0));
-	marker2Points3.push_back(Point3f(MARKER_SIZE,MARKER_SIZE,0));
-	marker2Points3.push_back(Point3f(0,MARKER_SIZE,0));
-	marker2Points3.push_back(Point3f(0,0,0));
+	marker2Points3.push_back(Point3f(MARKER_SIZE, 0, 0));
+	marker2Points3.push_back(Point3f(MARKER_SIZE, MARKER_SIZE, 0));
+	marker2Points3.push_back(Point3f(0, MARKER_SIZE, 0));
+	marker2Points3.push_back(Point3f(0, 0, 0));
 
 	vector<Point3f> marker3Points3;
-	marker3Points3.push_back(Point3f(MARKER_SIZE,MARKER_SIZE,0));
-	marker3Points3.push_back(Point3f(0,MARKER_SIZE,0));
-	marker3Points3.push_back(Point3f(0,0,0));
-	marker3Points3.push_back(Point3f(MARKER_SIZE,0,0));
+	marker3Points3.push_back(Point3f(MARKER_SIZE, MARKER_SIZE, 0));
+	marker3Points3.push_back(Point3f(0, MARKER_SIZE, 0));
+	marker3Points3.push_back(Point3f(0, 0, 0));
+	marker3Points3.push_back(Point3f(MARKER_SIZE, 0, 0));
 
 	vector<Point3f> marker4Points3;
-	marker4Points3.push_back(Point3f(0,MARKER_SIZE,0));
-	marker4Points3.push_back(Point3f(0,0,0));
-	marker4Points3.push_back(Point3f(MARKER_SIZE,0,0));
-	marker4Points3.push_back(Point3f(MARKER_SIZE,MARKER_SIZE,0));
+	marker4Points3.push_back(Point3f(0, MARKER_SIZE, 0));
+	marker4Points3.push_back(Point3f(0, 0, 0));
+	marker4Points3.push_back(Point3f(MARKER_SIZE, 0, 0));
+	marker4Points3.push_back(Point3f(MARKER_SIZE, MARKER_SIZE, 0));
 
 	markerPoints.push_back(marker1Points3);
 	markerPoints.push_back(marker2Points3);
 	markerPoints.push_back(marker3Points3);
 	markerPoints.push_back(marker4Points3);
 
-	dstPoints.push_back(Point2f(0,0));
-	dstPoints.push_back(Point2f(MARKER_SIZE,0));
-	dstPoints.push_back(Point2f(MARKER_SIZE,MARKER_SIZE));
-	dstPoints.push_back(Point2f(0,MARKER_SIZE));
+	dstPoints.push_back(Point2f(0, 0));
+	dstPoints.push_back(Point2f(MARKER_SIZE, 0));
+	dstPoints.push_back(Point2f(MARKER_SIZE, MARKER_SIZE));
+	dstPoints.push_back(Point2f(0, MARKER_SIZE));
 }
 
 void matchPoints(vector<Point2f> points, Mat colorImage, Mat dst, const Mat K, const Mat distCoef){
-	Mat homo = findHomography(points,dstPoints);
+	Mat homo = findHomography(points, dstPoints);
 	Mat warpHomo;
 
-	warpPerspective(colorImage,warpHomo,homo,Size(MARKER_SIZE,MARKER_SIZE));
+	warpPerspective(colorImage, warpHomo, homo, Size(MARKER_SIZE, MARKER_SIZE));
+	Mat compare;
 
-	for(int m=0; m < markers.size();m++){
-		Mat compare;
-		bitwise_xor(warpHomo,markers[m],compare);
-		int white=0;
-		for(int y=0; y < compare.size().height; y++){
-			for(int x=0; x < compare.size().width; x++){
-				if( compare.at<uchar>(y,x) == 0 ){
-					white++;
+	int maxDiff = MARKER_SIZE*MARKER_SIZE*(1 - ERROR_ALLOWED);
+	int count = maxDiff;
+	int mSize = markers.size();
+
+	for (int m = 0; m < mSize; m++){
+
+		bitwise_xor(warpHomo, markers[m], compare);
+		//int white = 0;
+		for (int y = 0; y < compare.size().height; y++){
+			for (int x = 0; x < compare.size().width; x++){
+				if (compare.at<uchar>(y, x) != 0){
+					//white++;
+					count--;
 				}
+				if (!count) break;
+			}
+			if (!count){
+				break;
 			}
 		}
 
 		//90% dos pixels iguais
-		if( white >= MARKER_SIZE*MARKER_SIZE*ERROR_ALLOWED ){
+		if (!count) count = maxDiff;
+		else if (count){
 			Mat rvec, tvec;
-			solvePnP(markerPoints[m], points, K, distCoef,rvec,tvec);
+			solvePnP(markerPoints[m], points, K, distCoef, rvec, tvec);
 
 			vector<Point3f> input;
-			input.push_back(Point3f(0,0,0)); input.push_back(Point3f(MARKER_SIZE,0,0)); input.push_back(Point3f(MARKER_SIZE,MARKER_SIZE,0)); input.push_back(Point3f(0,MARKER_SIZE,0));
-			input.push_back(Point3f(0,0,-MARKER_SIZE)); input.push_back(Point3f(MARKER_SIZE,0,-MARKER_SIZE)); input.push_back(Point3f(MARKER_SIZE,MARKER_SIZE,-MARKER_SIZE)); input.push_back(Point3f(0,MARKER_SIZE,-MARKER_SIZE));
+			input.push_back(Point3f(0, 0, 0)); input.push_back(Point3f(MARKER_SIZE, 0, 0)); input.push_back(Point3f(MARKER_SIZE, MARKER_SIZE, 0)); input.push_back(Point3f(0, MARKER_SIZE, 0));
+			input.push_back(Point3f(0, 0, -MARKER_SIZE)); input.push_back(Point3f(MARKER_SIZE, 0, -MARKER_SIZE)); input.push_back(Point3f(MARKER_SIZE, MARKER_SIZE, -MARKER_SIZE)); input.push_back(Point3f(0, MARKER_SIZE, -MARKER_SIZE));
 
 			vector<Point2f> imgPoints;
-			projectPoints(input,rvec,tvec,K,distCoef,imgPoints);
+			projectPoints(input, rvec, tvec, K, distCoef, imgPoints);
 
 			DrawingObject obj(imgPoints);
 			obj.draw(dst);
